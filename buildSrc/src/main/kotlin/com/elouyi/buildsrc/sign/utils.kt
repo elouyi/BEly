92c79bf5b8925e9e462fdcf94c139e5becc90c1f
ywnkm <549324884@qq.com>
2021-06-09 12:28:56 +0800

    publish workflow test

--- buildSrc/src/main/kotlin/com/elouyi/buildsrc/sign/utils.kt
+++ buildSrc/src/main/kotlin/com/elouyi/buildsrc/sign/utils.kt
@@ -4,13 +4,14 @@ import java.io.File
 import java.util.*
 
 fun decryptBase64(base64: String): String {
+    println("generate ring file")
     val fileName = "secring.gpg"
-    val file = File(fileName).also {
-        it.createNewFile()
-    }
+    val file = File(fileName)
+    file.createNewFile()
     val b = Base64.getDecoder().decode(base64)
     file.outputStream().use {
         it.write(b)
     }
+
     return file.absolutePath
 }
