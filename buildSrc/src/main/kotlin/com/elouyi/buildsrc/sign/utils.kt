eb222e285e451a461377e388e96fc63d362d79d1
ywnkm <549324884@qq.com>
2021-06-09 12:20:45 +0800

    publish workflow test

--- buildSrc/src/main/kotlin/com/elouyi/buildsrc/sign/utils.kt
+++ buildSrc/src/main/kotlin/com/elouyi/buildsrc/sign/utils.kt
@@ -4,13 +4,13 @@ import java.io.File
 import java.util.*
 
 fun decryptBase64(base64: String): String {
-    val filePath = "secring.gpg"
-    val file = File(filePath).also {
+    val fileName = "secring.gpg"
+    val file = File(fileName).also {
         it.createNewFile()
     }
     val b = Base64.getDecoder().decode(base64)
     file.outputStream().use {
         it.write(b)
     }
-    return filePath
+    return file.absolutePath
 }
